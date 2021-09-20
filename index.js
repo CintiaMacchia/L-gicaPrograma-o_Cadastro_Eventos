9e078b8049fc7cf300190de09e7290474fcb793e
Cintia <macchiapaola@hotmail.com>
2021-09-20 20:36:06 -0300

    alteração no cadastro de evento

--- index.js
+++ index.js
@@ -1,45 +1,36 @@
 //variaveis de data
-var data = new Date();
-var dia = String(data.getDate()).padStart(2, '0');
-var mes = String(data.getMonth() + 1).padStart(2, '0');
-var ano = data.getFullYear();
+var dataAtual = new Date();
+var dia = String(dataAtual.getDate()).padStart(2, '0');
+var mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
+var ano = dataAtual.getFullYear();
 dataAtual = dia + '/' + mes + '/' + ano;
 
 //variaveis do evento
-let dataEvento = "19/09/2021"
+
 let QuantPalestrantes = 5
 let QuantParticipantes = 85
 let totalpublico = QuantPalestrantes + QuantParticipantes
 
 //solicitar data do evento()
-console.log("Qual a data do Evento?")
-console.log(dataEvento)
-
-//solicitar idade
-console.log("Qual sua idade?")
-console.log (parseInt(Idade(1982)))
-
-//participantes
-console.log("Informe a quantidade de palestrantes e convidados?")
-console.log(`${QuantPalestrantes} Palestrantes e ${QuantParticipantes} Participantes(total:${totalpublico})`)
-
-function Idade(ano) {
-    return new Date().getFullYear() - ano;
- }
-
-//verificacoes
+let dataEvento = window.prompt("Qual a data do Evento?")
 if (dataEvento < dataAtual){
-    console.log("Evento não poderá ser cadastrado")
-    console.log("Motivo: Data Inválida")
-}  
-else if (Idade(1982) < 18){
-    console.log("Evento não poderá ser cadastrado")
-    console.log("Motivo: Cadasdtro não permitido para menores de 18 anos")
-}
-else if (totalpublico > 100){
-    console.log("Evento não poderá ser cadastrado")
-    console.log("Motivo: Limite de 100 pessoas excedido")
-}
-else{console.log("Cadastro efetuado com sucesso!")
+    window.alert("Evento não poderá ser cadastrado")
+    window.alert("Motivo: Data Inválida")
+}else{
+let anoNasc = window.prompt("Qual seu ano de nascimento?")
+let idade = ano - anoNasc
+if (idade < 18){
+    window.alert("Evento não poderá ser cadastrado")
+    window.alert("Motivo: Cadasdtro não permitido para menores de 18 anos")
+}else{
+window.alert(`${QuantPalestrantes} Palestrantes e ${QuantParticipantes} Participantes = total ${totalpublico}`)
 
+if (totalpublico > 100){
+    window.alert("Evento não poderá ser cadastrado")
+    window.alert("Motivo: Limite de 100 pessoas excedido")
+}
+    else{
+        window.alert("Cadastro efetuado com sucesso!")
+    }
+}
 }
